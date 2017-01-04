af76cfa9bc23608ce2ac2547fb525f6283be426d
fix <fx.thoorens@gmail.com>
2017-01-04 17:34:37 +0100

    exporting hd wallet

--- index.js
+++ index.js
@@ -4,7 +4,13 @@ ark = {
 	signature : require("./lib/transactions/signature.js"),
 	transaction : require("./lib/transactions/transaction.js"),
 	vote : require("./lib/transactions/vote.js"),
-	ipfs : require("./lib/transactions/ipfs.js")
+	ipfs : require("./lib/transactions/ipfs.js"),
+	wallet {
+		hdnode : require("./lib/hdnode.js"),
+		ecpair : require("./lib/ecpair.js"),
+		ecsignature : require("./lib/ecsignature.js")
+	}
+
 }
 
 module.exports = ark;

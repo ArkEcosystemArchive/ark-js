ee2cdf6eb2c5c22ef1b8e8ce6c3012febecaec2a
Brian Faust <hello@brianfaust.me>
2018-03-14 10:18:29 +0200

    es6

--- src/crypto/ecpair.js
+++ src/crypto/ecpair.js
@@ -17,6 +17,16 @@ const secp256k1 = ecurve.getCurveByName('secp256k1')
 
 const secp256k1native = require('secp256k1')
 
+// Object.defineProperty(ECPair.prototype, 'Q', {
+//   get: function () {
+//     if (!this.__Q && this.d) {
+//       this.__Q = secp256k1.G.multiply(this.d)
+//     }
+
+//     return this.__Q
+//   }
+// })
+
 /**
  * Provide either `d` or `Q` but not both.
  *
@@ -27,182 +37,172 @@ const secp256k1native = require('secp256k1')
  * @param {boolean} [options.compressed=true]
  * @param {Network} [options.network=networks.ark]
  */
-exports.ECPair = (d, Q, options) => {
-  if (options) {
-    typeforce({
-      compressed: types.maybe(types.Boolean),
-      network: types.maybe(types.Network)
-    }, options)
-  }
-
-  options = options || {}
-
-  if (d) {
-    if (d.signum() <= 0) throw new Error('Private key must be greater than 0')
-    if (d.compareTo(secp256k1.n) >= 0) throw new Error('Private key must be less than the curve order')
-    if (Q) throw new TypeError('Unexpected publicKey parameter')
-
-    this.d = d
-  } else {
-    typeforce(types.ECPoint, Q)
-
-    this.__Q = Q
-  }
-
-  /** @type {boolean} */
-  this.compressed = options.compressed === undefined ? true : options.compressed
-  /** @type {Network} */
-  this.network = options.network || NETWORKS.ark
-}
-
-Object.defineProperty(ECPair.prototype, 'Q', {
-  get: function () {
-    if (!this.__Q && this.d) {
-      this.__Q = secp256k1.G.multiply(this.d)
+module.exports = class ECPair {
+  constructor(d, Q, options) {
+    if (options) {
+      typeforce({
+        compressed: types.maybe(types.Boolean),
+        network: types.maybe(types.Network)
+      }, options)
     }
 
-    return this.__Q
-  }
-})
+    options = options || {}
 
-/**
- * @param {Buffer} buffer
- * @param {Network} [network=networks.ark]
- * @returns {ECPair}
- */
-ECPair.fromPublicKeyBuffer = (buffer, network) => {
-  var Q = ecurve.Point.decodeFrom(secp256k1, buffer)
+    if (d) {
+      if (d.signum() <= 0) throw new Error('Private key must be greater than 0')
+      if (d.compareTo(secp256k1.n) >= 0) throw new Error('Private key must be less than the curve order')
+      if (Q) throw new TypeError('Unexpected publicKey parameter')
 
-  return new ECPair(null, Q, {
-    compressed: Q.compressed,
-    network: network
-  })
-}
+      this.d = d
+    } else {
+      typeforce(types.ECPoint, Q)
 
-/**
- * @param {string} string
- * @param {Network[]|Network} network
- * @returns {ECPair}
- */
-ECPair.fromWIF = (string, network) => {
-  var decoded = wif.decode(string)
-  var version = decoded.version
+      this.__Q = Q
+    }
 
-  // [network, ...]
-  if (types.Array(network)) {
-    network = network.filter(function (network) {
-      return version === network.wif
-    }).pop()
-
-    if (!network) throw new Error('Unknown network version')
-
-    // network
-  } else {
-    network = network || NETWORKS.ark
-
-    if (version !== network.wif) throw new Error('Invalid network version')
+    /** @type {boolean} */
+    this.compressed = options.compressed === undefined ? true : options.compressed
+      /** @type {Network} */
+    this.network = options.network || NETWORKS.ark
   }
 
-  var d = BigInteger.fromBuffer(decoded.privateKey)
+  /**
+   * @param {Buffer} buffer
+   * @param {Network} [network=networks.ark]
+   * @returns {ECPair}
+   */
+  fromPublicKeyBuffer(buffer, network) {
+    var Q = ecurve.Point.decodeFrom(secp256k1, buffer)
 
-  return new ECPair(d, null, {
-    compressed: decoded.compressed,
-    network: network
-  })
-}
+    return new ECPair(null, Q, {
+      compressed: Q.compressed,
+      network: network
+    })
+  }
 
-/**
- * @param {object} [options]
- * @param {function} [options.rng]
- * @param {boolean} [options.compressed=true]
- * @param {Network} [options.network=networks.ark]
- */
-ECPair.makeRandom = (options) => {
-  options = options || {}
+  /**
+   * @param {string} string
+   * @param {Network[]|Network} network
+   * @returns {ECPair}
+   */
+  fromWIF(string, network) {
+    var decoded = wif.decode(string)
+    var version = decoded.version
 
-  var rng = options.rng || randomBytes
+    // [network, ...]
+    if (types.Array(network)) {
+      network = network.filter(function(network) {
+        return version === network.wif
+      }).pop()
 
-  var d
-  do {
-    var buffer = rng(32)
-    typeforce(types.Buffer256bit, buffer)
+      if (!network) throw new Error('Unknown network version')
 
-    d = BigInteger.fromBuffer(buffer)
-  } while (d.signum() <= 0 || d.compareTo(secp256k1.n) >= 0)
+      // network
+    } else {
+      network = network || NETWORKS.ark
 
-  return new ECPair(d, null, options)
-}
+      if (version !== network.wif) throw new Error('Invalid network version')
+    }
 
-/**
- * @param {string} seed
- * @param {object} [options]
- * @param {boolean} [options.compressed=true]
- * @param {Network} [options.network=networks.ark]
- * @returns {ECPair}
- */
-ECPair.fromSeed = (seed, options) => {
-  var hash = bcrypto.sha256(new Buffer(seed, 'utf-8'))
-  var d = BigInteger.fromBuffer(hash)
-  if (d.signum() <= 0 || d.compareTo(secp256k1.n) >= 0) {
-    throw new Error('seed cannot resolve to a compatible private key')
-  } else {
+    var d = BigInteger.fromBuffer(decoded.privateKey)
+
+    return new ECPair(d, null, {
+      compressed: decoded.compressed,
+      network: network
+    })
+  }
+
+  /**
+   * @param {object} [options]
+   * @param {function} [options.rng]
+   * @param {boolean} [options.compressed=true]
+   * @param {Network} [options.network=networks.ark]
+   */
+  makeRandom(options) {
+    options = options || {}
+
+    var rng = options.rng || randomBytes
+
+    var d
+    do {
+      var buffer = rng(32)
+      typeforce(types.Buffer256bit, buffer)
+
+      d = BigInteger.fromBuffer(buffer)
+    } while (d.signum() <= 0 || d.compareTo(secp256k1.n) >= 0)
+
     return new ECPair(d, null, options)
   }
+
+  /**
+   * @param {string} seed
+   * @param {object} [options]
+   * @param {boolean} [options.compressed=true]
+   * @param {Network} [options.network=networks.ark]
+   * @returns {ECPair}
+   */
+  fromSeed(seed, options) {
+    var hash = bcrypto.sha256(new Buffer(seed, 'utf-8'))
+    var d = BigInteger.fromBuffer(hash)
+    if (d.signum() <= 0 || d.compareTo(secp256k1.n) >= 0) {
+      throw new Error('seed cannot resolve to a compatible private key')
+    } else {
+      return new ECPair(d, null, options)
+    }
+  }
+
+  /**
+   * @returns {string}
+   */
+  getAddress() {
+    var payload = new Buffer(21)
+    var hash = bcrypto.ripemd160(this.getPublicKeyBuffer())
+    var version = this.getNetwork().pubKeyHash
+    payload.writeUInt8(version, 0)
+    hash.copy(payload, 1)
+
+    return base58check.encode(payload)
+  }
+
+  /**
+   * @returns {Network}
+   */
+  getNetwork() {
+    return this.network
+  }
+
+  getPublicKeyBuffer() {
+    return this.Q.getEncoded(this.compressed)
+  }
+
+  /**
+   * Requires a private key (`d`) to be set.
+   *
+   * @param {Buffer} hash
+   * @returns {ECSignature}
+   */
+  sign(hash) {
+    if (!this.d) throw new Error('Missing private key')
+    var native = secp256k1native.sign(hash, this.d.toBuffer(32))
+    return ECSignature.parseNativeSecp256k1(native).signature
+  }
+
+  /**
+   * Requires a private key (`d`) to be set.
+   *
+   * @returns {string}
+   */
+  toWIF() {
+    if (!this.d) throw new Error('Missing private key')
+
+    return wif.encode(this.network.wif, this.d.toBuffer(32), this.compressed)
+  }
+
+  /**
+   * @param {Buffer} hash
+   * @returns {boolean}
+   */
+  verify(hash, signature) {
+    return secp256k1native.verify(hash, signature.toNativeSecp256k1(), this.Q.getEncoded(this.compressed))
+  }
 }
-
-/**
- * @returns {string}
- */
-ECPair.prototype.getAddress = () => {
-  var payload = new Buffer(21)
-  var hash = bcrypto.ripemd160(this.getPublicKeyBuffer())
-  var version = this.getNetwork().pubKeyHash
-  payload.writeUInt8(version, 0)
-  hash.copy(payload, 1)
-
-  return base58check.encode(payload)
-}
-
-/**
- * @returns {Network}
- */
-ECPair.prototype.getNetwork = () => {
-  return this.network
-}
-
-ECPair.prototype.getPublicKeyBuffer = () => {
-  return this.Q.getEncoded(this.compressed)
-}
-
-/**
- * Requires a private key (`d`) to be set.
- *
- * @param {Buffer} hash
- * @returns {ECSignature}
- */
-ECPair.prototype.sign = (hash) => {
-  if (!this.d) throw new Error('Missing private key')
-  var native = secp256k1native.sign(hash, this.d.toBuffer(32))
-  return ECSignature.parseNativeSecp256k1(native).signature
-}
-
-/**
- * Requires a private key (`d`) to be set.
- *
- * @returns {string}
- */
-ECPair.prototype.toWIF = () => {
-  if (!this.d) throw new Error('Missing private key')
-
-  return wif.encode(this.network.wif, this.d.toBuffer(32), this.compressed)
-}
-
-/**
- * @param {Buffer} hash
- * @returns {boolean}
- */
-ECPair.prototype.verify = (hash, signature) => {
-  return secp256k1native.verify(hash, signature.toNativeSecp256k1(), this.Q.getEncoded(this.compressed))
-}
-
-module.exports = ECPair

14ed7b62aeeac33cfd4b5f87baf31516a082640e
Brian Faust <hello@brianfaust.me>
2018-03-16 14:19:34 +0200

    build constants in config

--- src/config.js
+++ src/config.js
@@ -1,12 +1,46 @@
+const deepmerge = require('deepmerge')
+
 class Config {
   setConfig (config) {
-    for (const [key, value] in config) {
-      this[key] = value
+    this.config = {}
+
+    for (const [key, value] of Object.entries(config)) {
+      this.config[key] = value
     }
+
+    this.buildConstants()
   }
 
   get (key) {
-    return this[key]
+    return this.config[key]
+  }
+
+  buildConstants () {
+    this.constants = this.network.constants.sort((a, b) => a.height - b.height)
+    this.constant = {
+      index: 0,
+      data: this.constants[0]
+    }
+
+    let lastmerged = 0
+
+    while (lastmerged < this.constants.length - 1) {
+      this.constants[lastmerged + 1] = deepmerge(this.constants[lastmerged], this.constants[lastmerged + 1])
+      lastmerged++
+    }
+  }
+
+  getConstants (height) {
+    while ((this.constant.index < this.constants.length - 1) && height >= this.constants[this.constant.index + 1].height) {
+      this.constant.index++
+      this.constant.data = this.constants[this.constant.index]
+    }
+    while (height < this.constants[this.constant.index].height) {
+      this.constant.index--
+      this.constant.data = this.constants[this.constant.index]
+    }
+
+    return this.constant.data
   }
 }
 

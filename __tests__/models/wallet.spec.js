d6485bab8e3a27c0e38d408fc7e198d943069021
jaml <dev@jaml.pro>
2018-03-29 21:53:40 +0200

    Fix some of the model tests

--- __tests__/models/wallet.spec.js
+++ __tests__/models/wallet.spec.js
@@ -5,6 +5,8 @@ import configManager from '@/managers/config'
 import network from '@/networks/devnet'
 
 describe('Models - Wallet', () => {
+  beforeEach(() => configManager.setConfig(network))
+
   describe('toString', () => {
     // TODO implementation is right?
     it('returns the address and the balance', () => {
@@ -18,10 +20,23 @@ describe('Models - Wallet', () => {
   })
 
   describe('apply transaction', () => {
-    it('should be ok for a multitx', () => {
-      const multitx = require('./fixtures/multi-transaction')
+    const testWallet = new Wallet('D61xc3yoBQDitwjqUspMPx1ooET6r1XLt7')
+    const data = {
+      publicKey: '02337316a26d8d49ec27059bd0589c49ba474029c3627715380f4df83fb431aece',
+      secondPublicKey: '020d3c837d0a47ee7de1082cd48885003c5e92964e58bb34af3b58c6e42208ae03',
+      balance: 109390000000,
+      vote: null,
+      username: null,
+      votebalance: 0,
+      multisignature: null,
+      dirty: false,
+      producedBlocks: 0,
+      missedBlocks: 0
+    }
 
-      expect(testWallet.canApply(multitx)).toBeTruthy()
+    it('should be ok for a multi-transaction', () => {
+      Object.keys(data).forEach(k => (testWallet[k] = data[k]))
+      expect(testWallet.canApply(multiTx)).toBeTruthy()
     })
   })
 })

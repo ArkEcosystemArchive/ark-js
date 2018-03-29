5c6fbb75c03307654932c1708811adccdb38e076
Brian Faust <hello@brianfaust.me>
2018-03-29 14:19:43 +0300

    second signature transaction

--- src/crypto/v2/transactions/second-signature.js
+++ src/crypto/v2/transactions/second-signature.js
@@ -3,7 +3,25 @@ import crypto from '../crypto'
 import slots from '../../crypto/slots'
 
 export default class SecondSignature {
+  constructor () {
+    this.id = null
+    this.type = 1
+    this.fee = Config.getConstants(height).fees.secondsignature
+    this.amount = 0
+    this.timestamp = slots.getTime()
+    this.recipientId = null
+    this.senderPublicKey = null
+    this.asset = { signature: {} }
+    this.version = 0x02
+    this.network = Config.all()
+  }
+
   create () {
+    return this
+  }
+
+  setPublicKeys (keys) {
+    this.senderPublicKey = keys.publicKey
     return this
   }
 
@@ -11,12 +29,15 @@ export default class SecondSignature {
     const keys = crypto.getKeys(passphrase)
     this.senderPublicKey = keys.publicKey
     this.signature = crypto.sign(this, keys)
+    this.asset.signature = this.signature
+    this.setPublicKeys(keys)
     return this
   }
 
   secondSign (transaction, passphrase) {
     const keys = crypto.getKeys(passphrase)
     this.secondSignature = crypto.secondSign(transaction, keys)
+    this.setPublicKeys(keys)
     return this
   }
 
@@ -29,7 +50,15 @@ export default class SecondSignature {
       hex: crypto.getBytes(this).toString('hex'),
       id: crypto.getId(this),
       signature: this.signature,
-      secondSignature: this.secondSignature
+      secondSignature: this.secondSignature,
+
+      type: this.type,
+      amount: this.amount,
+      fee: this.fee,
+      recipientId: this.recipientId,
+      senderPublicKey: this.senderPublicKey,
+      timestamp: this.timestamp,
+      asset: this.asset
     }
   }
 }

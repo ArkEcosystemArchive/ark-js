ae49e8e40c883dc248a13908449da68741e0f0d8
Brian Faust <hello@brianfaust.me>
2018-03-29 14:16:56 +0300

    ipfs transaction

--- src/crypto/v2/transactions/ipfs.js
+++ src/crypto/v2/transactions/ipfs.js
@@ -3,8 +3,34 @@ import crypto from '../crypto'
 import slots from '../../crypto/slots'
 
 export default class IPFS {
+  constructor () {
+    this.id = null
+    this.type = 5
+    this.fee = Config.getConstants(height).fees.ipfs
+    this.amount = 0
+    this.timestamp = slots.getTime()
+    this.vendorFieldHex = null
+    this.senderPublicKey = null
+    this.asset = {}
+    this.version = 0x02
+    this.network = Config.all()
+  }
+
   create (ipfshash) {
     this.ipfshash = ipfshash
+    return this
+  }
+
+  setVendorField (type) {
+    this.vendorFieldHex = Buffer.from(this.ipfshash, type).toString('hex')
+    while (this.vendorFieldHex.length < 128) {
+      this.vendorFieldHex = '00' + this.vendorFieldHex
+    }
+    return this
+  }
+
+  setPublicKeys (keys) {
+    this.senderPublicKey = keys.publicKey
     return this
   }
 
@@ -12,12 +38,14 @@ export default class IPFS {
     const keys = crypto.getKeys(passphrase)
     this.senderPublicKey = keys.publicKey
     this.signature = crypto.sign(this, keys)
+    this.setPublicKeys(keys)
     return this
   }
 
   secondSign (transaction, passphrase) {
     const keys = crypto.getKeys(passphrase)
     this.secondSignature = crypto.secondSign(transaction, keys)
+    this.setPublicKeys(keys)
     return this
   }
 
@@ -30,7 +58,14 @@ export default class IPFS {
       hex: crypto.getBytes(this).toString('hex'),
       id: crypto.getId(this),
       signature: this.signature,
-      secondSignature: this.secondSignature
+      secondSignature: this.secondSignature,
+
+      type: this.type,
+      amount: this.amount,
+      fee: this.fee,
+      senderPublicKey: this.senderPublicKey,
+      timestamp: this.timestamp,
+      asset: this.asset
     }
   }
 }

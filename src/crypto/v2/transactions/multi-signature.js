b7a1c8c434050c7a32169560627667f1a6a57d16
Brian Faust <hello@brianfaust.me>
2018-03-29 14:17:18 +0300

    multisignature transaction

--- src/crypto/v2/transactions/multi-signature.js
+++ src/crypto/v2/transactions/multi-signature.js
@@ -3,10 +3,34 @@ import crypto from '../crypto'
 import slots from '../../crypto/slots'
 
 export default class MultiSignature {
+  constructor () {
+    this.id = null
+    this.type = 4
+    this.fee = 0
+    this.amount = 0
+    this.timestamp = slots.getTime()
+    this.recipientId = null
+    this.senderPublicKey = null
+    this.asset = { multisignature: {} }
+    this.version = 0x02
+    this.network = Config.all()
+  }
+
   create (keysgroup, lifetime, min) {
-    this.keysgroup = keysgroup
-    this.lifetime = lifetime
-    this.min = min
+    this.asset.multisignature.keysgroup = keysgroup
+    this.asset.multisignature.lifetime = lifetime
+    this.asset.multisignature.min = min
+    this.setFee(keysgroup)
+    return this
+  }
+
+  setFee (keysgroup) {
+    this.fee = (keysgroup.length + 1) * Config.getConstants(height).fees.multisignature
+    return this
+  }
+
+  setPublicKeys (keys) {
+    this.senderPublicKey = keys.publicKey
     return this
   }
 
@@ -14,12 +38,14 @@ export default class MultiSignature {
     const keys = crypto.getKeys(passphrase)
     this.senderPublicKey = keys.publicKey
     this.signature = crypto.sign(this, keys)
+    this.setPublicKeys(keys)
     return this
   }
 
   secondSign (transaction, passphrase) {
     const keys = crypto.getKeys(passphrase)
     this.secondSignature = crypto.secondSign(transaction, keys)
+    this.setPublicKeys(keys)
     return this
   }
 
@@ -32,7 +58,15 @@ export default class MultiSignature {
       hex: crypto.getBytes(this).toString('hex'),
       id: crypto.getId(this),
       signature: this.signature,
-      secondSignature: this.secondSignature
+      secondSignature: this.secondSignature,
+
+      type: this.type,
+      amount: this.amount,
+      fee: this.fee,
+      recipientId: this.recipientId,
+      senderPublicKey: this.senderPublicKey,
+      timestamp: this.timestamp,
+      asset: this.asset
     }
   }
 }

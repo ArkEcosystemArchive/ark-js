76f9f3a0d0ea305688e91db4b36aff463645aac4
Brian Faust <hello@brianfaust.me>
2018-04-06 13:33:02 +0300

    set devnet as default config if none is provided

--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import ApiClient from '@/api'
 import transactionBuilder from '@/builder'
 import configManager from '@/managers/config'
 import feeManager from '@/managers/fee'
+import defaultConfig from '@/networks/ark/devnet'
 
 export default class Ark {
   constructor (config) {
-    this.setConfig(config)
+    this.setConfig(config ? config : defaultConfig)
   }
 
   setConfig (config) {
